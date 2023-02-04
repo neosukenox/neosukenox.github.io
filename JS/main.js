0c90e4c6d41e9b2f114d9f9d2e9e2194da38ba00
neosuke nox <neosuke.nox@gmail.com>
2023-02-04 07:34:37 -0300

    yanosequeponercommit

--- JS/main.js
+++ JS/main.js
@@ -14,5 +14,24 @@ function goTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+/*
+var ele = document.getElementsByClassName('show_hide');
+for (var i = 0; i < ele.length; i++){
+  ele[i].addEventListener('click',() => hideCategory(ele[i]));  
+}
+*/
 
-  
+
+function hideCategory2(a,b){
+  i = document.getElementById(a);
+  j= document.getElementById(b);
+  if(i.style.display == "none"){
+      i.style.display= "block"
+      j.value = "-";
+  }
+  else
+  {
+    i.style.display= "none"
+    j.value = "+";
+  }
+}
